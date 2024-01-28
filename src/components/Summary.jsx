8a582fa54f3688821b990f42b34f4b8e0dfde239
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-01-28 16:50:50 +0800

    create Summary component to display and stylize quiz results overview

---
+++ src/components/Summary.jsx
@@ -0,0 +1,64 @@
+import quizCompleteImg from "../assets/quiz-complete.png";
+import QUESTIONS from "../questions.js";
+
+export default function Summary({ userAnswers }) {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0]
+  );
+  const incorrectAnswers = userAnswers.filter(
+    (answer, index) => answer !== QUESTIONS[index].answers[0] && answer !== null
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+
+  const correctAnswersShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100
+  );
+
+  const incorrectAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
+
+  return (
+    <div id="summary">
+      <img src={quizCompleteImg} alt="Trophy Icon" />
+      <h2>Quiz Completed!</h2>
+      <div id="summary-stats">
+        <p>
+          <span className="number">{skippedAnswersShare}%</span>
+          <span className="text">skipped</span>
+        </p>
+        <p>
+          <span className="number">{correctAnswersShare}%</span>
+          <span className="text">answered correctly</span>
+        </p>
+        <p>
+          <span className="number">{incorrectAnswersShare}%</span>
+          <span className="text">answered incorrectly</span>
+        </p>
+      </div>
+      <ol>
+        {userAnswers.map((answer, index) => {
+          let cssClass = "user-answer ";
+
+          if (!answer) {
+            cssClass += "skipped";
+          } else if (answer === QUESTIONS[index].answers[0]) {
+            cssClass += "correct";
+          } else {
+            cssClass += "wrong";
+          }
+
+          return (
+            <li key={answer ?? QUESTIONS[index].id}>
+              <h3>{QUESTIONS[index].id}</h3>
+              <p className="question">{QUESTIONS[index].text}</p>
+              <p className={cssClass}>{answer ?? "Skipped"}</p>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+}
